3bc7c2b934592371c17d2ad304fd5a5d0b2d15f6
Dev4281 <divyansh.sharma0210@gmail.com>
2023-07-01 02:44:02 +0530

    real commit

--- Node-Server/index.js
+++ Node-Server/index.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
     username: String,
     email:String
   });
-  const User = mongoose.model('User', userSchema);2
+  const User = mongoose.model('User', userSchema);
 
 const server =express();
 server.use(cors());
